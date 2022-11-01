28c15749adbf15baf956831ab249f0eb5facad7c
Thiravijoy <112856232+Thiravijoy@users.noreply.github.com>
2022-11-01 22:17:46 +0530

    register component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { LoginModule } from './login/login.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    LoginModule
+    LoginModule,
+    RegisterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
